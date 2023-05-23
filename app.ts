9839c570256622c1e9db60c74a5fe57ff7e1498d
aleviannaf <alexandrevianna511@gmail.com>
2023-05-23 15:37:51 -0400

    app implementation

--- app.ts
+++ app.ts
@@ -1,13 +1,18 @@
 import { CompanyAccount } from './class/CompanyAccount'
+import { NewAccount } from './class/NewAccount'
 import { PeopleAccount } from './class/PeopleAccount'
 
-//const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-//console.log(peopleAccount)
-//peopleAccount.deposit(50)
-//peopleAccount.withdraw(70)
+const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
+console.log(peopleAccount)
+peopleAccount.deposit(50)
+peopleAccount.withdraw(70)
+peopleAccount.getBalance()
 
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
 companyAccount.deposit(10)
-//console.log(companyAccount)
 companyAccount.getLoan(5000)
 companyAccount.getBalance()
+
+const newAccount: NewAccount = new NewAccount("Alexandre", 30)
+newAccount.deposit(30)
+newAccount.getBalance()
